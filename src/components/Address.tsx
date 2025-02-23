71881fa45f7025fa1de3ff80337da3542e718fc0
usman <usman_qb@yahoo.com>
2025-02-23 20:54:34 +0500

    address section fields should appear in a row

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -7,19 +7,21 @@ const Address = () => {
   return (
     <section className="section z-10">
       <h1 className="section-heading mb-5">Address</h1>
-      <TextField
-        sx={{ marginRight: 5 }}
-        label="Country"
-        variant="standard"
-        value={address?.country}
-        onChange={(e) => updateCountry(e.target.value)}
-      />
-      <TextField
-        label="City"
-        variant="standard"
-        value={address?.city}
-        onChange={(e) => updateCity(e.target.value)}
-      />
+      <div className="flex">
+        <TextField
+          sx={{ marginRight: 5 }}
+          label="Country"
+          variant="standard"
+          value={address?.country}
+          onChange={(e) => updateCountry(e.target.value)}
+        />
+        <TextField
+          label="City"
+          variant="standard"
+          value={address?.city}
+          onChange={(e) => updateCity(e.target.value)}
+        />
+      </div>
     </section>
   );
 };
