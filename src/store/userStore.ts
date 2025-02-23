ce2828a6ca9617354d26e6847cc74744eff1e0b1
usman <usman_qb@yahoo.com>
2025-02-24 01:54:15 +0500

    state actions refactored to use immer

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,55 +1,54 @@
 import { create } from "zustand";
 import { UserState } from "./types/userStore";
+import { immer } from "zustand/middleware/immer";
 
-export const useUserStore = create<UserState>((set) => ({
-  name: "John",
-  age: 28,
-  address: {
-    city: "New York",
-    country: "USA",
-  },
-  friends: [
-    {
-      name: "Emily",
-      hobbies: ["biking", "music", "gaming"],
+export const useUserStore = create<UserState>()(
+  immer((set) => ({
+    name: "John",
+    age: 28,
+    address: {
+      city: "New York",
+      country: "USA",
     },
-    {
-      name: "Peter",
-      hobbies: ["soccer", "gaming"],
+    friends: [
+      {
+        name: "Emily",
+        hobbies: ["biking", "music", "gaming"],
+      },
+      {
+        name: "Peter",
+        hobbies: ["soccer", "gaming"],
+      },
+    ],
+    updateName: (name) => set(() => ({ name })),
+    updateAge: (age) => set(() => ({ age })),
+    updateCity: (city) =>
+      set((state) => {
+        state.address.city = city;
+      }),
+    updateCountry: (country) =>
+      set((state) => {
+        state.address.country = country;
+      }),
+    removeFriend: (index) => {
+      set((state) => {
+        state.friends.splice(index, 1);
+      });
     },
-  ],
-  updateName: (name) => set(() => ({ name })),
-  updateAge: (age) => set(() => ({ age })),
-  updateCity: (city) =>
-    set((state) => ({ ...state, address: { ...state.address, city } })),
-  updateCountry: (country) =>
-    set((state) => ({ ...state, address: { ...state.address, country } })),
-  removeFriend: (index) => {
-    set((state) => {
-      const newFriends = [...state.friends];
-      newFriends.splice(index, 1);
-      return { ...state, friends: newFriends };
-    });
-  },
-  updateFriendName: (friendIndex, friendName) => {
-    set((state) => {
-      const updatedFriends = [...state.friends];
-      updatedFriends[friendIndex].name = friendName;
-      return { ...state, friends: updatedFriends };
-    });
-  },
-  initFriend: () => {
-    set((state) => {
-      const updatedFriends = [...state.friends];
-      updatedFriends.push({ name: "", hobbies: [] });
-      return { ...state, friends: updatedFriends };
-    });
-  },
-  updateHobbies: (friendIndex, hobbies) => {
-    set((state) => {
-      const updatedFriends = [...state.friends];
-      updatedFriends[friendIndex].hobbies = hobbies;
-      return { ...state, friends: updatedFriends };
-    });
-  },
-}));
+    updateFriendName: (friendIndex, friendName) => {
+      set((state) => {
+        state.friends[friendIndex].name = friendName;
+      });
+    },
+    initFriend: () => {
+      set((state) => {
+        state.friends.push({ name: "", hobbies: [] });
+      });
+    },
+    updateHobbies: (friendIndex, hobbies) => {
+      set((state) => {
+        state.friends[friendIndex].hobbies = hobbies;
+      });
+    },
+  }))
+);
